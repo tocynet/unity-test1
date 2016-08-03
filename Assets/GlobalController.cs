df4208ec3884578d4712f0690acc13970e1715d8
unknown <t.nishiwaki@OFFICE-PC-078.central.super-elements.co.jp>
2016-08-03 19:13:50 +0900

    first commit

---
+++ Assets/GlobalController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalController : MonoBehaviour {
+
+    public static GlobalController instance;
+
+	// Use this for initialization
+	void Start () {
+	    if (instance == null)
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/WebCamBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebCamBehavior : MonoBehaviour {
+    public int Width = 1920;
+    public int Height = 1080;
+    public int FPS = 30;
+
+	// Use this for initialization
+	void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        if (devices.Length > 0)
+        {
+            var webcamTexture = new WebCamTexture(devices[0].name);
+            GetComponent<Renderer>().material.mainTexture = webcamTexture;
+            webcamTexture.Play();
+        } else
+        {
+            Debug.Log("カメラが見つかりませんでした。");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
