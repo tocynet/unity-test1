c3f1f4f5243efe0c47f00c285d4888b60d9a4464
Toshiharu <tocy.net@gmail.com>
2016-08-04 19:01:49 +0900

    showing message modified
    showing message modified

--- Assets/WebCamBehavior.cs
+++ Assets/WebCamBehavior.cs
@@ -7,36 +7,44 @@ public class WebCamBehavior : MonoBehaviour
     public int Width = 1920;
     public int Height = 1080;
     public int FPS = 30;
+    public static bool _inited = false;
 
-    private MsgText _msgText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _inited = false;
+        Debug.Log("WebCamBehavior Start()");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!_inited && MsgText.instance!=null)
+        {
+            WebCamDevice[] devices = WebCamTexture.devices;
+            if (devices.Length > 0)
+            {
+                var webcamTexture = new WebCamTexture(devices[0].name);
+                GetComponent<Renderer>().material.mainTexture = webcamTexture;
+                webcamTexture.Play();
+                MsgText.instance.msg = "カメラ・キャプチャ中";
+            }
+            else
+            {
+                Debug.Log("カメラが見つかりませんでした。");
+                MsgText.instance.msg = "カメラが見つかりませんでした。";
+            }
+            _inited = true;
+        }
+    }
+
 
     public void OnClick()
     {
         Debug.Log("Button Clicked!!");
         MsgText.instance.msg = "Button Clicked!!";
     }
-
-    // Use this for initialization
-    void Start ()
-    {
-        Debug.Log("WebCamBehavior");
-        WebCamDevice[] devices = WebCamTexture.devices;
-        if (devices.Length > 0)
-        {
-            var webcamTexture = new WebCamTexture(devices[0].name);
-            GetComponent<Renderer>().material.mainTexture = webcamTexture;
-            webcamTexture.Play();
-        } else
-        {
-            Debug.Log("カメラが見つかりませんでした。");
-        }
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-	
-	}
 
 
 }
